37263abb0a2dc39edeaab9c758d1ea74662b0fd2
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-09-18 16:37:01 -0700

    feat(movies.service): uses the queries and creates a getAllMovies method

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -1,58 +1,36 @@
-import { gql } from "@apollo/client";
 import { graphqlService, type GraphQLService } from "./graphql.service";
 import { Movie, MoviesQueryResponse } from "./movies.types";
+import { MOVIES_QUERY, ALL_MOVIES_QUERY } from "../constants/movies.queries";
 
 interface GetMoviesOptions {
-	page?: number
-	perPage?: number
+	page?: number;
+	perPage?: number;
 }
 
 export class MoviesService {
 	#defaultPage = 1;
 	#defaultPerPage = 10;
-
 	#graphqlService: GraphQLService;
 
 	constructor(graphqlService: GraphQLService) {
 		this.#graphqlService = graphqlService;
 	}
 
-	getPaginatedMovies(options?: GetMoviesOptions): Promise<Movie[]> {
+	async getPaginatedMovies(options?: GetMoviesOptions): Promise<Movie[]> {
 		const { page = this.#defaultPage, perPage = this.#defaultPerPage } = options ?? {};
 
-		return this.#graphqlService.query<MoviesQueryResponse>({
-			query: gql`
-			query Movies($pagination: PaginationInput) {
-				movies(pagination: $pagination) {
-					nodes {
-						id
-						title
-						posterUrl
-						summary
-						duration
-						directors
-						mainActors
-						datePublished
-						rating
-						ratingValue
-						bestRating
-						worstRating
-						writers
-						genres {
-							id
-							title
-						}
-					}
-				}
-			}`,
-			variables: {
-				pagination: { page, perPage },
-			},
-		}).then((response) => {
-			const { data } = response;
-
-			return data?.movies.nodes ?? [];
+		const { data } = await this.#graphqlService.query<MoviesQueryResponse>({
+			query: MOVIES_QUERY,
+			variables: { pagination: { page, perPage } },
 		});
+
+		return data?.movies.nodes ?? [];
+	}
+
+	async getAllMovies(): Promise<Movie[]> {
+		const { data } = await this.#graphqlService.query<MoviesQueryResponse>({ query: ALL_MOVIES_QUERY });
+
+		return data?.movies.nodes ?? [];
 	}
 }
 
