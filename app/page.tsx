9d84d523bf7886b878f5b7cb01eef4504f502445
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-09-18 16:59:23 -0700

    feat(index): normalizes button stylings and fixes search by using the new method

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,46 @@
 import Link from "next/link";
-import Image from "next/image";
 import { moviesService } from "@/src/services/movies.service";
 import MovieCard from "@/src/components/movie-card";
 
-export default async function Home({ searchParams }: { searchParams: { search?: string; page?: string } }) {
-	const query = searchParams.search ?? "";
-	const page = Number(searchParams.page ?? "1");
+const buttonStyles = "px-4 py-2 rounded-md bg-gray-800 text-white hover:bg-gray-700 transition-colors";
 
-	const movies = await moviesService.getPaginatedMovies({ page, perPage: 10 });
+export default async function Home({ searchParams }: { searchParams: { search?: string; page?: string } }) {
+	const query = await searchParams.search ?? "";
+	const page = await Number(searchParams.page ?? "1");
+
+	const movies = query
+		? await moviesService.searchMovies(query)
+		: await moviesService.getPaginatedMovies({ page, perPage: 10 });
 
 	const total = movies.length ?? 0;
 
 	return (
 		<div className="p-4">
-			<h1 className="text-2xl font-bold mb-4">Movies</h1>
+			{query && <Link href="/" className={`${buttonStyles} inline-block my-5`}>Go Back</Link>}
 
-			<div className="flex items-center justify-between my-10">
-				{page > 1 ? (
-					<Link href={`/?search=${encodeURIComponent(query)}&page=${page - 1}`} className="px-4 py-2 rounded-md bg-gray-800 text-white hover:bg-gray-700 transition-colors">
-						← Previous
+			<h1 className="text-2xl font-bold mb-4">
+				{query ? `Showing ${total} results for "${query}"` : "Movies"}
+			</h1>
+
+			{!query && (
+				<div className="flex items-center justify-between my-10">
+					{page > 1 ? (
+						<Link href={`/?page=${page - 1}`} className={buttonStyles}>
+							Previous
+						</Link>
+					) : (
+						<div />
+					)}
+
+					<span className="px-4 py-2 bg-gray-100 rounded-md text-gray-700 font-medium shadow-sm">
+						Page {page} - Total Results = {total}
+					</span>
+
+					<Link href={`/?page=${page + 1}`} className={buttonStyles}>
+						Next
 					</Link>
-				) : (
-					<div />
-				)}
-
-				<span className="px-4 py-2 bg-gray-100 rounded-md text-gray-700 font-medium shadow-sm">
-					Page {page}
-				</span>
-
-				<Link href={`/?search=${encodeURIComponent(query)}&page=${page + 1}`} className="px-4 py-2 rounded-md bg-gray-800 text-white hover:bg-gray-700 transition-colors">
-					Next →
-				</Link>
-			</div>
-
+				</div>
+			)}
 
 			{movies.length > 0 ? (
 				<div className="grid gap-6 grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
