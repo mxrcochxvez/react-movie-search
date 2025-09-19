704357a0201ab96e6064b131dc81291b573f7ce4
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-09-18 22:14:46 -0700

    feat(bearer-token): uses singleton pattern for service

--- src/services/bearer-token.service.ts
+++ src/services/bearer-token.service.ts
@@ -2,22 +2,26 @@ export interface BearerTokenResponse {
 	token: string;
 }
 
-export const bearerTokenService = {
-	getBearerToken,
+export class BearerTokenService {
+	private readonly endpoint = 'https://0kadddxyh3.execute-api.us-east-1.amazonaws.com/auth/token';
+
+	async getBearerToken(): Promise<string> {
+		const response = await fetch(this.endpoint);
+
+		if (!response.ok) {
+			throw new Error(
+				`Failed to fetch bearer token: ${response.status} ${response.statusText}`
+			);
+		}
+
+		const data = (await response.json()) as BearerTokenResponse;
+
+		if (!data?.token) {
+			throw new Error('Invalid token response');
+		}
+
+		return data.token;
+	}
 }
 
-async function getBearerToken(): Promise<string> {
-	const repsonse = await fetch('https://0kadddxyh3.execute-api.us-east-1.amazonaws.com/auth/token');
-
-	if (!repsonse.ok) {
-		throw new Error(`Failed to fetch bearer token: ${repsonse.status} ${repsonse.statusText}`);
-	}
-
-	const data = (await repsonse.json()) as BearerTokenResponse;
-
-	if (!data?.token) {
-		throw new Error('Invalid token response');
-	}
-
-	return data.token;
-}
+export const bearerTokenService = new BearerTokenService();
